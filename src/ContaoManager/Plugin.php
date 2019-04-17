b2ba8525a14746a1b90dc8333f34d3afe16eea6f
Martin Hauke <hauke@september-werbeagentur.de>
2019-04-17 12:45:48 +0000

    Make module available in Contao admin panel
    Add backend route, menu entry and dummy template with basic translation;

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -6,10 +6,13 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use SeptemberWerbeagentur\ContaoRealEstateBundle\ContaoRealEstateBundle;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 
-class Plugin implements BundlePluginInterface
+class Plugin implements BundlePluginInterface, RoutingPluginInterface
 {
     /**
      * {@inheritdoc}
@@ -21,4 +24,12 @@ class Plugin implements BundlePluginInterface
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
+
+    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
+    {
+        $file = __DIR__.'/../Resources/config/routing.yml';
+
+        return $resolver->resolve($file)->load($file);
+    }
 }
+
---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_HOOKS']['getUserNavigation'][] = ['real_estate.navigation_listener', 'onGetUserNavigation'];
