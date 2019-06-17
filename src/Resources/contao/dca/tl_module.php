567d7e9fab032f6fe56efa11ea54eb680eaabf23
sox <gitlab@simplel.de>
2019-06-17 12:05:21 +0200

    Remove obsolete code (backend field)

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -3,13 +3,3 @@
 // Add palette to tl_module
 $GLOBALS['TL_DCA']['tl_module']['palettes']['propertylist'] = '{title_legend},name,type,jumpTo;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['september_realEstate_jumpTo'] = array
-(
-   'label'                   => &$GLOBALS['TL_LANG']['tl_faq_category']['jumpTo'],
-	'exclude'                 => true,
-	'inputType'               => 'pageTree',
-	'foreignKey'              => 'tl_page.title',
-	'eval'                    => array('fieldType'=>'radio', 'tl_class'=>'clr'),
-	'sql'                     => "int(10) unsigned NOT NULL default 0",
-	'relation'                => array('type'=>'hasOne', 'load'=>'lazy')
-);
