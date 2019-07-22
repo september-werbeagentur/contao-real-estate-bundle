8c7cb598e92cd0d18a540c4e879e60c0983ceb99
sox <gitlab@simplel.de>
2019-07-22 15:31:15 +0200

    Add translations and optimize texts

---
+++ src/Resources/contao/languages/de/tl_realestate_objects.php
@@ -0,0 +1,19 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_realestate_objects']['edit'][0] = 'Bearbeiten';
+$GLOBALS['TL_LANG']['tl_realestate_objects']['edit'][1] = 'Wohnungen dieses Objekts bearbeiten';
+$GLOBALS['TL_LANG']['tl_realestate_objects']['editheader'][0] = 'Bearbeiten';
+$GLOBALS['TL_LANG']['tl_realestate_objects']['editheader'][1] = 'Objekt mit ID %s bearbeiten';
+$GLOBALS['TL_LANG']['tl_realestate_objects']['copy'][0] = 'Kopieren';
+$GLOBALS['TL_LANG']['tl_realestate_objects']['copy'][1] = 'Objekt mit ID %s kopieren';
+$GLOBALS['TL_LANG']['tl_realestate_objects']['delete'][0] = 'Löschen';
+$GLOBALS['TL_LANG']['tl_realestate_objects']['delete'][1] = 'Objekt mit ID %s löschen';
+$GLOBALS['TL_LANG']['tl_realestate_objects']['show'][0] = 'Anzeigen';
+$GLOBALS['TL_LANG']['tl_realestate_objects']['show'][1] = 'Details anzeigen';
+
+$GLOBALS['TL_LANG']['tl_realestate']['data_legend'] = 'Objektdaten';
+$GLOBALS['TL_LANG']['tl_realestate']['details_legend'] = 'Objektdetails';
+$GLOBALS['TL_LANG']['tl_realestate_objects']['name'][0] = 'Name';
+$GLOBALS['TL_LANG']['tl_realestate_objects']['name'][1] = 'Name des Objektes';
+$GLOBALS['TL_LANG']['tl_realestate_objects']['description'][0] = 'Beschreibung (optional)';
+$GLOBALS['TL_LANG']['tl_realestate_objects']['description'][1] = 'Geben Sie optional eine Beschreibung zum Objekt an.';
